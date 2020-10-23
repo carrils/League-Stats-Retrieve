b4477b99df28dce706f8c7c4e5401ed3d63dc779
Sam Carrillo <scarrillo578@gmail.com>
2020-10-22 23:58:12 -0500

    minus coolhackytricks

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/*
-TODO:
-move cool hacky tricks.java to the repo you made for it on github
- */
-
 public class Main extends Application {
 
     @Override
