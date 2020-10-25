b81efe55d08519f1f21334250c9bcb8be4176d56
Sam Carrillo <scarrillo578@gmail.com>
2020-10-25 00:23:46 -0500

    finish up 1.0 design on layout.fxml

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,16 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    public static void main(String[] args) {
+        //Create scraper which contains functions needed to interact with GUI
+        launch(args);
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Layout.fxml"));
         primaryStage.setTitle("League of Legends stats retrieve 1.0");
-        primaryStage.setScene(new Scene(root, 575, 400));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-        //Create scraper which contains functions needed to interact with GUI
-        launch(args);
     }
 }
