4c1087dfa766ea662677f9036eee6d0d675f39fa
Sam Carrillo <scarrillo578@gmail.com>
2020-11-01 12:14:45 -0600

    add style.css to remove splitpane border, change color scheme

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,10 @@ public class Main extends Application {
         ToDo:
         - make a .exe for this
         - get riot key that wont expire in 24 hours lol
+        - find better color scheme or better font color
+        - make info button functional to provide information on project
+        - clear textbox after submission to chain queries easier
+        - make default region NA
      */
     public static void main(String[] args) {
         //Create scraper which contains functions needed to interact with GUI
@@ -17,9 +21,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        //get GUI from fxml file "Layout"
         Parent root = FXMLLoader.load(getClass().getResource("Layout.fxml"));
+        //set title of window on application
         primaryStage.setTitle("League of Legends stats retrieve 1.0");
+        //create scene on stage 'root' with width x height
         primaryStage.setScene(new Scene(root, 600, 400));
+        //make scene 'root' reference stylesheet 'style.css'
+        root.getStylesheets().add(String.valueOf(Main.class.getResource("style.css")));
+        //show the stage
         primaryStage.show();
     }
 }
